3b181a8aa8c389a1d2579a79973211e640902138
Bobby Singh <b830103@gmail.com>
2024-01-11 20:04:00 +0100

    Implement isConnected prop in Chat.js for source selection
    In "Chat.js", utilize the isConnected prop to decide what source to fetch data from.
    
    If there’s a connection, fetch messages from the Firestore database and cache messages whenever possible in an onSnapshot callback function (involves AsyncStorage.setItem()).
    Otherwise, load cached messages from the local storage (involves AsyncStorage.getItem()).

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   collection,
@@ -9,8 +9,14 @@ import {
   onSnapshot,
   orderBy,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, isConnected, db, navigation }) => {
+  const loadCachedLists = async () => {
+    const cachedLists = (await AsyncStorage.getItem("messages")) || [];
+    setLists(JSON.parse(cachedLists));
+  };
+
   const { name, backgroundColor, bubbleColors, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
@@ -33,6 +39,26 @@ const Chat = ({ route, navigation, db }) => {
       }}
     />
   );
+
+  //to push messages to storage
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  //to get messages from storage
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  //to hide input field when user offline
+  const renderInputToolBar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
 
   // Set the navigation title
   useEffect(() => {
@@ -60,24 +86,34 @@ const Chat = ({ route, navigation, db }) => {
   //   ]);
   // }, []);
 
+  let unsubMessages;
   useEffect(() => {
-    navigation.setOptions({ title: name });
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()),
+    // when there is connection fetch data from db otherwise fetch from AsyncStorage
+    if (isConnected === true) {
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when useEffect code is re-executed
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      navigation.setOptions({ title: name });
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+        let newMessages = [];
+        documentsSnapshot.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
         });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
+    } else loadCachedMessages();
+
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, []); // tested with [isConnected] as dependency not working
 
   // Keyboard Avoiding View for different platforms
   const keyboardAvoidingViewProps = Platform.select({
@@ -90,6 +126,7 @@ const Chat = ({ route, navigation, db }) => {
       style={[styles.container, { backgroundColor: backgroundColor || "#fff" }]}
     >
       <GiftedChat
+        renderInputToolbar={renderInputToolBar}
         messages={messages}
         onSend={onSend}
         user={{ _id: userID, name: name }}
